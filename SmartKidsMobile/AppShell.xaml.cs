b142b4d67da01daab1f71cb84e6f61554360738a
Deyvid Rannyere <deyran@gmail.com>
2023-08-21 16:42:26 -0300

    SmartKidsMobile

---
+++ SmartKidsMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartKidsMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SmartKidsMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SmartKidsMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
