536f984bbc36d4279af5ca3a10a2cf30e6483607
Deyvid Rannyere <deyran@gmail.com>
2023-08-22 16:26:27 -0300

    Curso Maui .Net

---
+++ SmartKidsMobile/obj/Debug/net7.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("SmartKidsMobile.MainApplication, SmartKidsMobile, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64fdb797f7536516cd.MainApplication.class, crc64fdb797f7536516cd.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
